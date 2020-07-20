7232efe24803a8a30c6562157c1d30e2b1b1072d
Vipul Swarup <vipul.swarup@eisenvault.com>
2020-07-20 17:45:49 +0530

    Parametrized code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-To use this project, please install Python 3 and Google Cloud Command Line tools
+To use this project, please install Node.js, Python 3 and Google Cloud Command Line tools
 
+To run, use the below on commandline
+`node index.js`
--- index.js
+++ index.js
@@ -1,58 +1,55 @@
+/**
+ * TODO(developer): Specify the target language code below
+ * This script auto-detects source language
+ */
+var target = "ar"; //Arabic
 
-//Get API Key from text file
-var fs = require('fs');
+/**
+ * TODO(developer): Specify the source and targe JSON file paths
+ */
+var targetJSON = "test-ar.json";
+var sourceJSON = "test-en.json";
 
-
-
+var fs = require("fs");
 
 // Imports the Google Cloud client library
-const {Translate} = require('@google-cloud/translate').v2;
+const { Translate } = require("@google-cloud/translate").v2;
 
 // Creates a client
 const translate = new Translate();
 
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
-
-var desktopEN = JSON.parse(fs.readFileSync("desktop-en.json", "UTF-8"));
+var desktopEN = JSON.parse(fs.readFileSync(sourceJSON, "UTF-8"));
 console.log(desktopEN);
-var desktopTranslated={};
-
-var target="ar"; //Arabic
+var desktopTranslated = {};
 
 translateText();
-
-
-
 
 async function translateText() {
   // Translates the text into the target language. "text" can be a string for
   // translating a single piece of text, or an array of strings for translating
   // multiple texts.
-  for (var key in desktopEN){
-  	let [translations] = await translate.translate(desktopEN[key], target);
-  	translations = Array.isArray(translations) ? translations : [translations];
-  	console.log('Translations:');
-  	translations.forEach((translation, i) => {
-  		desktopTranslated[key]=translation;
-    	console.log(`${desktopEN[key]} => (${target}) ${translation}`);
-  	});
+  for (var key in desktopEN) {
+    let [translations] = await translate.translate(desktopEN[key], target);
+    translations = Array.isArray(translations) ? translations : [translations];
+    console.log("Translations:");
+    translations.forEach((translation, i) => {
+      desktopTranslated[key] = translation;
+      console.log(`${desktopEN[key]} => (${target}) ${translation}`);
+    });
   }
   console.log(desktopTranslated);
   writeJSONToFile(desktopTranslated);
 }
 
-function writeJSONToFile(jsonObj){
-	// convert JSON object to string
-const data = JSON.stringify(jsonObj,null, 4);
+function writeJSONToFile(jsonObj) {
+  // convert JSON object to string
+  const data = JSON.stringify(jsonObj, null, 4);
 
-// write JSON string to a file
-fs.writeFile('desktop-es.json', data, (err) => {
+  // write JSON string to a file
+  fs.writeFile(targetJSON, data, (err) => {
     if (err) {
-        throw err;
+      throw err;
     }
     console.log("JSON data is saved.");
-});
+  });
 }
-
